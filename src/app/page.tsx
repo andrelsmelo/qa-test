6c3eced414509781c749a7a72929ed0cac92a9bf
andrelsmelo <andrelucassmelo@gmail.com>
2023-06-16 18:07:48 -0300

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,94 +1,53 @@
 import Image from 'next/image'
-import styles from './page.module.css'
+import './globals.css';
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="main-container">
+      <div className="planeta-esquerda">
+        <Image src={'/planeta-esquerda.png'} alt={'planeta-esquerda'} width={190} height={190} />
+      </div>
+      <div>
+        <div className="form-container">
+          <form action="">
+            <div className="form-header">
+              <Image src={'/logo-form.png'} alt={'logo-form'} width={110} height={45} />
+            </div>
+            <div className="form-body">
+              <label htmlFor="nome">Nome:</label>
+              <input type="text" id="nome" name="nome" />
+              <label htmlFor="email">Email:</label>
+              <input type="email" id="email" name="email" />
+              <div className="form-row">
+                <div className="form-column">
+                  <label htmlFor="login">Login:</label>
+                  <input type="text" id="login" name="login" />
+                </div>
+                <div className="form-column">
+                  <label htmlFor="senha">Senha :</label>
+                  <input type="password" id="senha" name="senha" />
+                </div>
+              </div>
+              <label htmlFor="cpf">CPF:</label>
+              <input type="text" id="cpf" name="cpf" />
+              <label htmlFor="genero">Genero:</label>
+              <select id="genero" name="genero">
+                <option value="masculino">Masculino</option>
+                <option value="feminino">Feminino</option>
+                <option value="outro">outro</option>
+              </select>
+            </div>
+            <p>
+              <a href="#">Ja tem uma conta?</a>
+            </p>
+            <div className="form-footer">
+              <button type="submit">Cadastrar</button>
+            </div>
+          </form>
         </div>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className="planeta-direita">
+        <Image src={'/planeta-direita.png'} alt={'planeta-direita'} width={190} height={190} />
       </div>
     </main>
   )
